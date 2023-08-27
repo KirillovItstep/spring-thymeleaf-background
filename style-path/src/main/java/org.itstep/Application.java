c9c9dbdf9ad3624667e49f7402ff89631e34a26a
kirillovItStep <kirillov.malp@gmail.com>
2023-08-27 18:44:19 +0300

    thymeleaf

---
+++ style-path/src/main/java/org.itstep/Application.java
@@ -0,0 +1,22 @@
+package org.itstep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
+
+@Controller
+class IndexController {
+
+    @GetMapping({"/", ""})
+    public String index() {
+        return "index";
+    }
+}
